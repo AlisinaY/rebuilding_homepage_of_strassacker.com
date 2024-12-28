86728e8769d59fba79474cb9eb3377603d0bee97
AlisinaY <yozbashi.a.y@gmail.com>
2024-12-28 20:13:10 +0100

    Header Component improved

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,61 +1,53 @@
-import React from "react";
-
 import "./Header.css";
 
 const Header = () => {
   return (
-    <>
-      <div className="d-flex header-container">
-        <div className="pos-relative header">
-          <div className="header__video">
-            <video autoPlay muted loop>
-              <source
-                src="https://www.strassacker.com/s3/1014-(1)(1)-1733399858.mp4"
-                type="video/mp4"
-              />
-            </video>
+    <div className="d-flex header-container">
+      <div className="pos-relative header">
+        <div className="header__video">
+          <div>
+            <video
+              autoPlay
+              muted
+              loop
+              type="video/mp4"
+              src="https://www.strassacker.com/s3/1014-(1)(1)-1733399858.mp4"
+            />
           </div>
-          <div className="pos-absolute d-flex flex-align-center header__heading">
-            <div className="d-flex flex-column header__items">
-              <div className="d-flex flex-align-center header__item">
-                <div className="header__sideline">
-                  <div></div>
-                </div>
-                <div>
-                  <h3 className="text-uppercase">Kunstgieserei Strassacker</h3>
-                </div>
+        </div>
+        <div className="d-flex flex-align-center flex-row pos-absolute header__title">
+          <div className="header__items">
+            <div className="d-flex flex-align-center header__item">
+              <div className="header__sideline">
+                <div></div>
               </div>
               <div>
-                <span className="header__title">
-                  Traditionelles Handwerk, moderne
+                <span
+                  data-content="Kunstgießerei Strassacker"
+                  className="text-uppercase">
+                  Kunstgießerei Strassacker
                 </span>
               </div>
-              <div>
-                <span className="header__title">Fertigungstechnik</span>
-              </div>
             </div>
-            <div className="background pos-absolute"></div>
-            <div className=" pos-absolute d-flex flex-align-center flex-column header__sideboxes">
-              <div className="d-flex flex-content-center header__sidebox">
-                <div>
-                  <span className="text-uppercase">sakral</span>
-                </div>
-              </div>
-              <div className="d-flex flex-content-center header__sidebox">
-                <div>
-                  <span className="text-uppercase">edition</span>
-                </div>
-              </div>
-              <div className="d-flex flex-content-center header__sidebox">
-                <div>
-                  <span className="text-uppercase">manufaktur</span>
-                </div>
-              </div>
+            <div className="header__item">
+              <h1>Traditionelles Handwerk, moderne Fertigungstechnik</h1>
             </div>
+          </div>
+          <div className="d-flex flex-column pos-relative header__sideboxes">
+            <div className="d-flex flex-align-center flex-content-center header__sidebox">
+              <div className="text-uppercase">sakral</div>
+            </div>
+            <div className="d-flex flex-align-center flex-content-center header__sidebox">
+              <div className="text-uppercase">editition</div>
+            </div>
+            <div className="d-flex flex-align-center flex-content-center header__sidebox">
+              <div className="text-uppercase">manufaktur</div>
+            </div>
+            <div className="pos-absolute background"></div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
