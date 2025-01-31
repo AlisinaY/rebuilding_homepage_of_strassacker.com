d08f96b6bfe54ea132a91f5aa2594afb886c22e7
AlisinaY <yozbashi.a.y@gmail.com>
2025-01-31 18:30:31 +0100

    Galleryshowcase component has responsive design for mobile and tablet

--- src/components/GalleryShowcase/GalleryShowcase.jsx
+++ src/components/GalleryShowcase/GalleryShowcase.jsx
@@ -3,6 +3,24 @@ import React, { useState } from "react";
 import { Button } from "../StrassackerMenu/Button";
 import "./GalleryShowcase.css";
 
+const data = [
+  {
+    gtitle: "Galerie",
+    imgScr:
+      "https://www.strassacker.com/img/containers/assets/galerie_neu/2023-11-21_strassacker_tobias-froehner-photography_0417-hdr.jpg/eecc0cd0138a6f6a647c4826b53e11fb.webp"
+  },
+  {
+    gtitle: "showroom",
+    imgScr:
+      "https://www.strassacker.com/img/containers/assets/galerie_neu/galerie-3.1.jpg/6f9fbf326c6c73edaed81c98c7bb6623.webp"
+  },
+  {
+    gtitle: "Ausstellung",
+    imgScr:
+      "https://www.strassacker.com/img/containers/assets/galerie_neu/galerie-1-1706610338.jpg/e054808f772258e7544bd7b2527faaa9.webp"
+  }
+];
+
 export default function GalleryShowcase() {
   const [hovered, setHovered] = useState(null);
 
@@ -17,6 +35,45 @@ export default function GalleryShowcase() {
   return (
     <>
       <div className="d-flex flex-align-center flex-column gs__container">
+        {/* Gallery Showcase Second Part from gs__cotainer class design for mobile and tablet */}
+        <div className="gs-main">
+          <div className="gs-main__header">
+            <div className="gs-main__title">
+              <div className="gs-main__sideline"></div>
+              <div className="gs-main__titledesc">
+                <span className="text-uppercase">ihr besuch bei uns</span>
+              </div>
+            </div>
+            <div className="gs-main__heading">
+              <h4>Galerien Strassacker</h4>{" "}
+            </div>
+            <div className="gs-main__txt">
+              <p>
+                Erkunden Sie unsere Galerien in Süßen und erleben Sie die
+                Schönheit der Kunst des Gießens in ihrer reinsten Form.
+              </p>
+            </div>
+          </div>
+          <div className="gs-main__imgcontainer">
+            {data.map((item) => {
+              return (
+                <div className="gs-main__items">
+                  <div className="gs-main__item">
+                    <div className="gs-main__img--header">
+                      <div className="gs-main__img--heading">
+                        <h3 className="text-uppercase"> {item.gtitle} </h3>
+                      </div>
+                      <div className="gs-main__img--sideline"></div>
+                    </div>
+                    <div className="gs-main__img--image">
+                      <img src={item.imgScr} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
         {/* Gallery Showcase First Part from gs__container class */}
         <div className="d-flex flex-column gs__container-title">
           <div className="d-flex flex-align-center gs__container-text">
@@ -44,25 +101,10 @@ export default function GalleryShowcase() {
             </div>
           </div>
         </div>
+
         {/* Gallery Showcase Second Part from gs__cotainer class*/}
         <div className="d-flex flex-align-center gs__container-menu">
-          {[
-            {
-              gtitle: "Galerie",
-              imgScr:
-                "https://www.strassacker.com/img/containers/assets/galerie_neu/2023-11-21_strassacker_tobias-froehner-photography_0417-hdr.jpg/eecc0cd0138a6f6a647c4826b53e11fb.webp"
-            },
-            {
-              gtitle: "showroom",
-              imgScr:
-                "https://www.strassacker.com/img/containers/assets/galerie_neu/galerie-3.1.jpg/6f9fbf326c6c73edaed81c98c7bb6623.webp"
-            },
-            {
-              gtitle: "Ausstellung",
-              imgScr:
-                "https://www.strassacker.com/img/containers/assets/galerie_neu/galerie-1-1706610338.jpg/e054808f772258e7544bd7b2527faaa9.webp"
-            }
-          ].map((item, i) => {
+          {data.map((item, i) => {
             return (
               <div
                 onMouseOver={() => handleMouseOver(i)}
